8e3ff75b8571096a2a0e4f170efd32a0325e5d51
Shiba <62843852+5h1b@users.noreply.github.com>
2025-01-24 19:37:39 -0500

    Update protonprep-valve-staging.sh

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -341,10 +341,16 @@
 
     echo "WINE: -CUSTOM- Downgrade MESSAGE to TRACE to remove write_watches spam"
     patch -Np1 < ../patches/proton/0001-ntdll-Downgrade-using-kernel-write-watches-from-MESS.patch
-
+    
     echo "WINE: -CUSTOM- Fix wine bug #56653 - GetLogicalProcessorInformation can be missing Cache information"
     patch -Np1 < ../patches/wine-hotfixes/pending/wine-bug-56653.patch
-
+    
+    echo "WINE: -Haptic Feedback Linux- patch1"
+    patch -Np1 < ../patches/haptic-feedback-linux/patch1.patch
+    
+    echo "WINE: -Haptic Feedback Linux- patch3"
+    patch -Np1 < ../patches/haptic-feedback-linux/patch3.patch
+    
     popd
 
 ### END PROTON-GE ADDITIONAL CUSTOM PATCHES ###
